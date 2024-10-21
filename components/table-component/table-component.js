2d5ed28d08073a26df53c8017701cade9578e166
juliantyh <juliantyh@gmail.com>
2024-10-21 14:50:59 +0800

    filling in headers and initial stuff

--- components/table-component/table-component.js
+++ components/table-component/table-component.js
@@ -1,11 +1,13 @@
 new gridjs.Grid({
-  columns: ["Name", "Email", "Phone Number"],
+  columns: ["Acronym", "Definition"],
   data: [
-    ["John", "john@example.com", "(353) 01 222 3333"],
-    ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
-    ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
-    ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
-    ["Afshin", "afshin@mail.com", "(353) 22 87 8356"],
+    ["SGH", "Singapore General Hospital"],
+    ["CGH", "Changi General Hospital"],
+    ["ESD", "Endoscopic Submucosal Dissection"],
+    ["EMR", "Endoscopic Mucosal Resection"],
+    ["ESR", "Endoscopic Submucosal Resection"],
+    ["EFTR", "Endoscopic Full Thickness Resection"],
+    ["FTRD", "Full Thickness Resection Device"],
   ],
 }).render(document.getElementById("table-1"));
 
